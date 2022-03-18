8a80ede62206e60ace73d0b53286931796948f3f
Izunyaaan <kanebluerivers@gmail.com>
2022-03-19 00:29:54 +0800

    Test

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,32 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-main>
+    <v-container fluid>
+      <div id="plot1" ref="plot1"></div>
+      <v-btn @click="test">Test</v-btn>
+    </v-container>
+  </v-main>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Plotly from "plotly.js-dist";
 
-nav {
-  padding: 30px;
-}
+export default {
+  name: "App",
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    //
+    count: 0,
+    data: [{ x: [1, 2, 3, 4], y: [10, 15, 13, 17], type: "scatter" }],
+    attr: { displayModeBar: true },
+    layout: { title: "My graph :)" },
+  }),
+  methods: {
+    test() {
+      Plotly.newPlot("plot1", this.data);
+    },
+  },
+  mounted() {
+    this.test();
+  },
+};
+</script>
